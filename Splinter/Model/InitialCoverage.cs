0eee3cb9f3a25d24cc9557058526c4ee3ed53a45
Jan Slavetinsky <tartaros@seznam.cz>
2015-04-14 02:30:23 +0200

    Adding first data objects

---
+++ Splinter/Model/InitialCoverage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splinter.Model
+{
+    /// <summary>
+    /// This represents the coverage as we sniffed it from the first run. This is created/filled by the "CoverageRunner" component.
+    /// We should be able to tell which subject methods are being tested by which test.
+    /// </summary>
+    public class InitialCoverage
+    {
+        public InitialCoverage(SessionSettings settings)
+        {
+            this.SessionSettings = settings;
+        }
+
+        SessionSettings SessionSettings { get; private set; }
+    }
+}
---
+++ Splinter/Model/SessionSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splinter.Model
+{
+    /// <summary>
+    /// Represents the initial situation - configuration that has either gone in via cmd line or we inferred it from files that are present.
+    /// Here we store what are the test subject assemblies and tester assemblies, and what test runner(s?) is being used.
+    /// </summary>
+    class SessionSettings
+    {
+
+        
+    }
+}

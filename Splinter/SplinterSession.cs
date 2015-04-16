ad26725ae8648d5b8c20cc9e2a663bf1b195972c
Jan Slavetinsky <tartaros@seznam.cz>
2015-04-17 01:48:18 +0200

    SplinterSession class shall manage the whole run of the app

---
+++ Splinter/SplinterSession.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Splinter.Model;
+
+using log4net;
+
+namespace Splinter
+{
+    public interface ISplinterSession
+    {
+
+        SessionSettings Initialize(string[] args);
+    }
+
+    public class SplinterSession : ISplinterSession
+    {
+        ILog log;
+
+        IPluginsContainer plugins;
+
+        public SplinterSession(ILog log, IPluginsContainer plugins)
+        {
+            this.plugins = plugins;
+            this.log = log;
+        }
+
+        public SessionSettings Initialize(string[] args)
+        {
+            if (!plugins.TestRunners.EmptyIfNull().Any())
+            {
+                //th
+            }
+
+            return null;
+        }
+    }
+}

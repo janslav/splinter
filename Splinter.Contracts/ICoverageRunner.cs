8ad587fcb3ff7205aedf9dac2d26dab760cd0391
Jan Slavetinsky <tartaros@seznam.cz>
2015-04-16 23:54:55 +0200

    Adding Contracts project. Thie extensibility interfaces will be here.

---
+++ Splinter.Contracts/ICoverageRunner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splinter.Contracts
+{
+    public interface ICoverageRunner
+    {
+        /// <summary>
+        /// Returns true if the runner is available, i.e. has its binaries installed, registered, etc.
+        /// </summary>
+        bool IsAvailable();
+
+
+    }
+}
---
+++ Splinter.Contracts/ICoverageRunnerMetadata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splinter.Contracts
+{
+    public interface ICoverageRunnerMetadata
+    {
+        /// <summary>
+        /// Returns the name of the coverage engine, such as "opencover".
+        /// </summary>
+        string Name { get; }
+    }
+}
